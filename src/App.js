fc6260f08706ab6d74fd35cdc50c2ada8b0e339d
Andrew <andrewmgunn26@gmail.com>
2022-08-17 10:01:41 -0500

    uibut

---
+++ src/App.js
@@ -0,0 +1,39 @@
+// Repository:  nugatory-react
+// Author:      Jeff Grissom
+// Version:     2.xx
+import React, { Component } from 'react';
+import Word from './components/Word';
+import Counter from './components/Counter';
+import './App.css';
+
+class App extends Component {
+  state = {
+    words: [
+      { id: 1, word: 'banana', color: 'yellow' },
+      { id: 2, word: 'apple', color: 'red' },
+      { id: 3, word: 'lime', color: 'green' },
+    ]
+  }
+  handleDelete = (wordId) => {
+    const words = this.state.words.filter(w => w.id !== wordId);
+    this.setState({ words:words });
+  }
+  render() { 
+    return ( 
+      <div className="App">
+        <header className="App-header">
+          nugatory
+        </header>
+        { this.state.words.map(word => 
+          <Word 
+            key={ word.id } 
+            word={ word } 
+            onDelete={ this.handleDelete } />
+        )}
+        <Counter totalWords={ this.state.words.length } />
+      </div>
+     );
+  }
+}
+ 
+export default App;
---
+++ src/components/Word.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Word extends Component {
+  // helper method
+  renderColor(color) {
+    return (color === undefined || color === null ? 'black' : color);
+  }
+  render() { 
+    const { onDelete, word } = this.props;
+    return (
+      <div onClick={ () => onDelete(word.id) } className='Word' style={{ color:this.renderColor(word.color) }}>
+        { word.word }
+      </div>
+    );
+  }
+}
+
+export default Word
